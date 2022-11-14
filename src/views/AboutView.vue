a1b9b75cc87ba3fe0f6416fcae29ad0932d02cc0
Euntaek13 <euntaekc13@gmail.com>
2022-11-14 09:52:43 +0900

    first

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div></div>
 </template>
+
+<script>
+import * as THREE from 'three'
+
+export default {
+  name: 'About',
+  mounted() {
+    console.log(THREE)
+  }
+}
+</script>
+
+<style></style>
